8897798898f27c8d9713f36b9b67c1d4845ec0fe
eliascharbelsalameh <eliascharbel.salameh@gmail.com>
2024-10-11 18:15:26 +0300

    Optimization of the code
    updated TODOs, removed useless features

--- DL_Chatbot_RAG.py
+++ DL_Chatbot_RAG.py
@@ -24,8 +24,12 @@ from langdetect import detect, LangDetectException # type: ignore
 # TODO: add memory
 # TODO: add new chat
 # TODO: enhance tables
+# TODO: load file online
 
 used_model = "gpt-4-turbo"
+chunk_size = 750
+chunk_overlap = 75
+temperature = 0.1
 
 # Initialize pygame mixer for TTS playback
 pygame.mixer.init()
@@ -221,8 +225,8 @@ if selection == "Chat with Amanda":
             return None  # Avoid proceeding if no documents are loaded
 
         text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=500,
-            chunk_overlap=50,
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
             separators=["\n\n", "\n", ".", " "]
         )  
         texts = text_splitter.split_documents(documents)
@@ -265,7 +269,7 @@ if selection == "Chat with Amanda":
         try:
             vectorstore = setup_vector_store(input_directory)
             template = """
-            Use the following pieces of context to answer the question at the end.
+            Use the following pieces of context to answer the question at the end. 
             If you don't know the answer, just say that you don't know, don't try to make up an answer.
             {context}
             Question: {question}
@@ -273,7 +277,7 @@ if selection == "Chat with Amanda":
 
             PROMPT = PromptTemplate(template=template, input_variables=["context", "question"])
             qa_chain = RetrievalQA.from_chain_type(
-                llm=ChatOpenAI(temperature=0, model_name=used_model),
+                llm=ChatOpenAI(temperature=temperature, model_name=used_model),
                 chain_type="stuff",
                 retriever=vectorstore.as_retriever(),
                 return_source_documents=True,
@@ -429,10 +433,7 @@ elif selection == "User Feedback":
 # Chunks Page: Display chunk settings and all available chunks
 elif selection == "All Chunks":
     st.title("Document Chunks")
-    chunk_size = 500
-    chunk_overlap = 50
-    st.write(f"**Chunk Size:** {chunk_size}")
-    st.write(f"**Chunk Overlap:** {chunk_overlap}")
+    st.write(f"**Chunk Size:** {chunk_size}, **Chunk Overlap:** {chunk_overlap}")
 
     if st.session_state["chunks"]:
         for idx, chunk in enumerate(st.session_state["chunks"]):
